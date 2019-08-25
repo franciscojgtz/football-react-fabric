54a0d20fb42a58240c1131e8f3ae2ff3bc6481ba
Francisco Gutierrez <francisco.j.gtz@gmail.com>
2019-08-25 12:30:39 -0700

    feat: get standings from server

--- src/Components/StandingsTableList/StandingsTableList.tsx
+++ src/Components/StandingsTableList/StandingsTableList.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { DetailsList } from "office-ui-fabric-react";
-import { worker } from "cluster";
+
 export interface IStandingsTableListItem {
   key: number;
   name: string;
+  position: string;
   crestUrl: string;
   points: number;
   playedGames: number;
@@ -33,6 +34,48 @@ export class StandingsTableList extends React.Component<
       items: []
     };
   }
+
+  private async getItems(leagueId: number) {
+    const response = await fetch(
+      `http://api.football-data.org/v2/competitions/${leagueId}/standings`,
+      {
+        headers: { "X-Auth-Token": "06ce053959a04bb8adf57acba0586981" }
+      }
+    );
+
+    return response.json();
+  }
+
+  async componentDidMount() {
+    const response = await this.getItems(2014).then(items => items);
+    let items: IStandingsTableListItem[] = [];
+    const standingsTable =
+      (response.standings &&
+        response.standings[0] &&
+        response.standings[0].table) ||
+      [];
+    for (const standing of standingsTable) {
+      const item = {
+        key: standing.team.id,
+        name: standing.team.name,
+        position: standing.position,
+        crestUrl: standing.team.crestUrl,
+        points: standing.points,
+        playedGames: standing.playedGames,
+        won: standing.won,
+        draw: standing.draw,
+        lost: standing.lost,
+        goalsFor: standing.goalsFor,
+        goalsAgainst: standing.goalsAgainst,
+        goalDifference: standing.goalDifference
+      };
+
+      items.push(item);
+    }
+
+    this.setState({ items });
+  }
+
   public render(): JSX.Element {
     const { items } = this.state;
     return <DetailsList items={items} />;
