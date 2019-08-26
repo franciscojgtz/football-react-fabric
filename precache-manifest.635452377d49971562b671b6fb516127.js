49d1406af203e70fd0ffd9b22a841d50b633ad91
Francisco Gutierrez <francisco.j.gtz@gmail.com>
2019-08-26 12:51:43 -0700

    Updates

---
+++ precache-manifest.635452377d49971562b671b6fb516127.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d8fd2fab931190e2cb278bb8037c239",
+    "url": "/football-react-fabric/index.html"
+  },
+  {
+    "revision": "bcb6f22fb9f6cd29b849",
+    "url": "/football-react-fabric/static/css/main.e54a84cf.chunk.css"
+  },
+  {
+    "revision": "2f39208d2a34f5a98b7e",
+    "url": "/football-react-fabric/static/js/2.e90c337f.chunk.js"
+  },
+  {
+    "revision": "bcb6f22fb9f6cd29b849",
+    "url": "/football-react-fabric/static/js/main.89c8afa3.chunk.js"
+  },
+  {
+    "revision": "10748363c5d29bf81dd6",
+    "url": "/football-react-fabric/static/js/runtime~main.810825b5.js"
+  }
+]);
